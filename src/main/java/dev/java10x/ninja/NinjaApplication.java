b65c730b7fd6c9a991455db3ace0c285a1f66b5d
arroz <gambiarragabriel@gmail.com>
2025-11-05 19:56:45 -0300

    first commit

---
+++ src/main/java/dev/java10x/ninja/NinjaApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.ninja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinjaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/ninja/NinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.ninja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
