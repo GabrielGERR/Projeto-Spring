a97ce0cbc1cbbfc16d861d1e17f3d5703f28c9a6
arroz <gambiarragabriel@gmail.com>
2025-11-10 21:13:23 -0300

    criando uma entidade no banco de dados

---
+++ src/main/java/dev/java10x/ninja/ninjaModel.java
@@ -0,0 +1,47 @@
+package dev.java10x.ninja;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "TD_cadastro")
+public class ninjaModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String Email;
+    private int idade;
+
+    public ninjaModel() {
+    }
+
+    public ninjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        Email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
