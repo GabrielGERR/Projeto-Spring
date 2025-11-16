348a0cc129e424889ac10b1fb6691f7dd33b8c51
arroz <gambiarragabriel@gmail.com>
2025-11-16 19:58:15 -0300

    fix:coloca a coluna Email como unico

--- src/main/java/dev/java10x/ninja/Ninja/ninjaModel.java
+++ src/main/java/dev/java10x/ninja/Ninja/ninjaModel.java
@@ -2,50 +2,29 @@ package dev.java10x.ninja.Ninja;
 
 import dev.java10x.ninja.missoes.missoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "TD_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ninjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
+
     private String nome;
+    @Column(unique = true)
     private String Email;
+
     private int idade;
+
     @ManyToOne
     @JoinColumn(name = "missoes_id")
     private missoesModel missoesModel;
 
-    public ninjaModel() {
-    }
-
-    public ninjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        Email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return Email;
-    }
-
-    public void setEmail(String email) {
-        Email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
--- src/main/java/dev/java10x/ninja/missoes/missoesModel.java
+++ src/main/java/dev/java10x/ninja/missoes/missoesModel.java
@@ -2,18 +2,28 @@ package dev.java10x.ninja.missoes;
 
 import dev.java10x.ninja.Ninja.ninjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "TB_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class missoesModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private long id;
     private String nome;
     private String Difuculdade;
 
+
     @OneToMany(mappedBy = "missoes")
     private List<ninjaModel> ninjaModel;
+
+
 }
