2cbc6c09d43b180d3dd2f0f0996ac1134c8671e4
arroz <gambiarragabriel@gmail.com>
2025-11-25 21:55:41 -0300

    feat: atualiza controllerNinja

--- src/main/java/dev/java10x/ninja/Ninja/ControllerNinja.java
+++ src/main/java/dev/java10x/ninja/Ninja/ControllerNinja.java
@@ -1,9 +1,6 @@
 package dev.java10x.ninja.Ninja;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -12,5 +9,34 @@ public class ControllerNinja {
     @GetMapping("/boasVindas")
     public String boavindas() {
         return "Olá jovem";
+    }
+    //Adicionar ninja
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "ninja criardo";
+    };
+    //mostrar todos ninjas
+    @GetMapping("/todos")
+    public String mostrarTodosNinjas() {
+        return "mostrar Ninja";
+    };
+
+    //procurar por id
+    @GetMapping("/todosID")
+    public String mostrarTodosNinjasPorId() {
+        return "Mostrar Ninja por id";
+    };
+
+    //alterar dados do ninja
+    @PostMapping("/alterarID")
+    public String update(){
+        return "alterar ninja por id";
+    }
+
+
+    //deletar ninja
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorId(){
+        return "deletarNinhja";
     }
 }
