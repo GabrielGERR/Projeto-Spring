34b1faed94158a49499a1dcba2eddff4bbe3901d
arroz <gambiarragabriel@gmail.com>
2025-11-25 22:10:32 -0300

    feat: criar as rotas do controllerMissoes com o mock

--- src/main/java/dev/java10x/ninja/missoes/MissoesController.java
+++ src/main/java/dev/java10x/ninja/missoes/MissoesController.java
@@ -1,4 +1,33 @@
 package dev.java10x.ninja.missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
 public class MissoesController {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "você esta no centro de missões ninjas";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "missão criada";
+    }
+
+    @GetMapping("/todos")
+    public String mostrarMissao(){
+        return "mostrar missões";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "missão alterada";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissao(){
+        return "missão deletada";
+    }
 }
